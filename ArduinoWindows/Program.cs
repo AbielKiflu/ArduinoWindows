b75e4496a337de7ce2b24e04430efa22d13cf19d
Abiel Kiflu <abiel.kiflu.weld@gmail.com>
2023-07-09 04:44:53 +0200

    Project setup

---
+++ ArduinoWindows/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ArduinoWindows
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
