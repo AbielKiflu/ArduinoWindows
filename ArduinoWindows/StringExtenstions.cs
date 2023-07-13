5f171d4b2c9457e39452460f708185494360fff2
Abiel Kiflu <abiel.kiflu.weld@gmail.com>
2023-07-13 21:29:03 +0200

    extension method added to handle string operation

---
+++ ArduinoWindows/StringExtenstions.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArduinoWindows
+{
+    public static class StringExtenstions
+    {
+
+        public static int GetDeviceValue(this string value)
+        {
+            int val = -1;
+            //Todo
+            // -100 = format exception
+            // -101 = undefined
+
+            string[] valueSplited=value.Split(',');
+            if (valueSplited.Length == 2)
+            {
+                try
+                {
+                    val = int.Parse(valueSplited[1]);
+                    return val;
+                }
+                finally
+                {
+                    val= -100;
+                     
+                }
+            }
+            return -1;
+        }
+
+
+
+ 
+
+    }
+
+}
