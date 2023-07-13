41585823ae458fa4b96c2ddab826dc54def4dce4
Abiel Kiflu <abiel.kiflu.weld@gmail.com>
2023-07-14 00:58:47 +0200

    refactoring

---
+++ ArduinoWindows/ArduinoCommunication.cs
@@ -0,0 +1,61 @@
+﻿using System.IO.Ports;
+
+
+namespace ArduinoWindows
+{
+    internal class ArduinoCommunication : IArduinoCommunication
+    {
+
+        private SerialPort serialPort;
+        public bool IsConnected => serialPort.IsOpen;
+
+        public ArduinoCommunication()
+        {
+            serialPort = new SerialPort();
+            serialPort.DataReceived += OnDataReceived;
+        }
+
+        public void Connect(string portName, int baudRate)
+        {
+            if (!serialPort.IsOpen)
+            {
+                serialPort.PortName = portName;
+                serialPort.BaudRate = baudRate;
+                serialPort.Open();
+            }
+        }
+
+        public void Disconnect()
+        {
+            if (serialPort.IsOpen)
+            {
+                serialPort.Close();
+                
+            }
+        }
+
+        public void SendCommand(string command)
+        {
+            if (serialPort.IsOpen)
+            {
+                serialPort.WriteLine(command);
+            }
+        }
+
+
+
+
+        private void OnDataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            string data = serialPort.ReadLine();
+            DataReceived?.Invoke(this, data);
+        }
+
+        public event EventHandler<string> DataReceived;
+
+
+    }
+}
+
+
+ 
---
+++ ArduinoWindows/IArduinoCommunication.cs
@@ -0,0 +1,16 @@
+﻿
+
+namespace ArduinoWindows
+{
+    public interface IArduinoCommunication
+    {
+        bool IsConnected { get; }
+        void Connect(string portName, int baudRate);
+        void Disconnect();
+        void SendCommand(string command);
+        event EventHandler<string> DataReceived;
+    }
+}
+
+
+ 
